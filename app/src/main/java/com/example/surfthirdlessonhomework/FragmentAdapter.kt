51841ed68700e5d2863cc23bbd422e6231ed152e
Wasilewski6860 <trollivanovich6860@gmail.com>
2024-04-09 10:43:26 +0300

    FragmentStatePagerAdapter used

---
+++ app/src/main/java/com/example/surfthirdlessonhomework/FragmentAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.surfthirdlessonhomework
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import java.util.Locale
+
+
+class FragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    private val fragmentList: List<Fragment> = listOf<Fragment>(
+        UpcomingFragment(),
+        LaunchesFragment(),
+        RocketsFragment()
+    )
+    private val titleList: List<String> = listOf( "Upcoming", "Launches", "Rockets")
+
+    override fun getCount(): Int = titleList.size
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titleList[position]
+    }
+
+}
--- app/src/main/java/com/example/surfthirdlessonhomework/MainActivity.kt
+++ app/src/main/java/com/example/surfthirdlessonhomework/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.surfthirdlessonhomework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.example.surfthirdlessonhomework.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(R.layout.activity_main) {
+
+    private lateinit var fragmentAdapter: FragmentAdapter
+
+    // reflection API is used under the hood
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fragmentAdapter = FragmentAdapter(supportFragmentManager)
+        binding.pager.adapter = fragmentAdapter
+        binding.tabLayout.setupWithViewPager(binding.pager);
+
     }
 }
